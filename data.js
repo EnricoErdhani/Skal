50fdd2d63b935fe06466087ab22c57ee2624b26f
EnricoErdhani <2118073@scholar.itn.ac.id>
2023-10-25 12:19:52 +0700

    [BAB 3] State and Props

---
+++ data.js
@@ -0,0 +1,65 @@
+export const sliderImages = [
+    {
+        id: 1,
+        image: 'https://mmc.kalteng.go.id/files/berita/27072023082815_0.jpeg',
+        title: 'Pameran Seni Rupa PEKAT Kembali',
+        description: 'Baca selengkapnya',
+    },
+    {
+        id: 2,
+        image: 'https://akcdn.detik.net.id/community/media/visual/2019/11/03/d5163ba9-8750-4b1b-9cf1-5ad989f1c25a.jpeg?w=700&q=90',
+        title: '24 Tarian Daerah Kalimantan',
+        description: 'Baca selengkapnya',
+    },
+    {
+        id: 3,
+        image: 'https://hasanzainuddin.files.wordpress.com/2008/09/7.jpg?w=300',
+        title: 'Sembilan Kerajinan Tangan Khas Kalimantan Selatan',
+        description: 'Baca selengkapnya',
+    },
+    {
+        id: 4,
+        image: 'https://gni.kemdikbud.go.id/pameran-virtual/images/restart/karya/8b4aebe7-3d5d-11ec-9202-e049c8140af6.webp?imwidth=1920',
+        title: 'Taman Budaya Kalimantan Utara',
+        description: 'Baca selengkapnya',
+    },
+];
+
+export const flatlist = [
+    {
+        id: 1,
+        image: 'https://nativeeyetravel.com/wp-content/uploads/2019/07/1-Hidden-Borneo-itinerary-slider-1920x880_result.jpg',
+        title: 'Hidden Borneo Kekayaan Tersembunyi Kaltim Lewat Seni Rupa',
+        description: 'Baca selengkapnya',
+    },
+    {
+        id: 2,
+        image: 'https://imgx.sonora.id/crop/0x0:0x0/700x465/photo/2021/03/20/pameranjpg-20210320033330.jpg',
+        title: 'Pameran Seni Rupa Pamerkan 52 Karya Seni Lukis Seniman Kalbar',
+        description: 'Baca selengkapnya',
+    },
+    {
+        id: 3,
+        image: 'https://asset-2.tstatic.net/banjarmasin/foto/bank/images/taman-budaya-kalsel-dialog.jpg',
+        title: 'Majukan Seni Rupa Kalsel, Taman Budaya Gelar Dialog Bersama Seniman',
+        description: 'Baca selengkapnya',
+    },
+    {
+        id: 4,
+        image: 'https://akcdn.detik.net.id/visual/2020/12/16/ini-sape-alat-musik-tradisional-suku-dayak-kalimantan-1_169.jpeg?w=750&q=90',
+        title: 'Ragam Alat Musik Tradisional Kalimantan yang Bisa Dikenalkan ke Anak',
+        description: 'Baca selengkapnya',
+    },
+    {
+        id: 5,
+        image: 'https://asset-a.grid.id//crop/0x0:0x0/700x465/photo/2021/11/18/1024px-jatung_utang-1-1jpg-20211118121745.jpg',
+        title: '7 Alat Musik Tradisional Kalimantan, dari Babun sampai Sluding',
+        description: 'Baca selengkapnya',
+    },
+    {
+        id: 6,
+        image: 'https://kebudayaan.kemdikbud.go.id/ditwdb/wp-content/uploads/sites/9/2019/11/Sapeq-Kalimantan-Timur-213.jpg',
+        title: 'Sapeq Kalimantan Timur, Salah satu alat musik tradisional suku Dayak',
+        description: 'Baca selengkapnya',
+    },
+];
